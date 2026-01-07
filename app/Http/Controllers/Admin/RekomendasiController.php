e33dd65af222579dd84c5b3607eee3ca7367102b
Samuel Bosawer <zam.bosawer@gmail.com>
2026-01-07 16:45:18 +0900

    fix

--- app/Http/Controllers/Admin/RekomendasiController.php
+++ app/Http/Controllers/Admin/RekomendasiController.php
@@ -14,91 +14,90 @@ class RekomendasiController extends Controller
     // Tampilkan semua data
     public function index()
     {
-       $kriteria   = Kriteria::orderBy('id')->get();
-$alternatif = Alternatif::orderBy('id')->get();
+        $kriteria   = Kriteria::orderBy('id')->get();
+        $alternatif = Alternatif::orderBy('id')->get();
 
-$nilai = PengaduanMasyarakat::with('nilaiKasus')
-    ->when(auth()->check() && auth()->user()->hasRole('masyarakat'), function ($query) {
-        $query->where('user_id', auth()->id());
-    })
-    ->get();
+        $nilai = PengaduanMasyarakat::with('nilaiKasus')
+            ->when(auth()->check() && auth()->user()->hasRole('masyarakat'), function ($query) {
+                $query->where('user_id', auth()->id());
+            })
+            ->get();
 
-$konversi     = Konversi::pluck('bobot_w', 'nilai_gap');
-$konversi_all = Konversi::orderBy('id')->get();
+        $konversi     = Konversi::pluck('bobot_w', 'nilai_gap');
+        $konversi_all = Konversi::orderBy('id')->get();
 
-$hasil = [];
+        $hasil = [];
 
-foreach ($nilai as $kasus) {
-    foreach ($alternatif as $alt) {
+        foreach ($nilai as $kasus) {
+            foreach ($alternatif as $alt) {
 
-        $cf = [];
-        $sf = [];
+                $cf = [];
+                $sf = [];
 
-        foreach ($kriteria as $k) {
+                foreach ($kriteria as $k) {
 
-            $nilaiKasus = $kasus->nilaiKasus
-                ->where('alternatif_id', $k->id)
-                ->first();
+                    $nilaiKasus = $kasus->nilaiKasus
+                        ->where('alternatif_id', $k->id)
+                        ->first();
 
-            if (!$nilaiKasus) continue;
+                    if (!$nilaiKasus) continue;
 
-            $gap   = $nilaiKasus->nilai_kasus - $alt->nilai_ideal_alternatif;
-            $bobot = $konversi[$gap] ?? 0;
+                    $gap   = $nilaiKasus->nilai_kasus - $alt->nilai_ideal_alternatif;
+                    $bobot = $konversi[$gap] ?? 0;
 
-            if ($k->jenis_faktor === 'Core') {
-                $cf[] = $bobot;
-            } else {
-                $sf[] = $bobot;
+                    if ($k->jenis_faktor === 'Core') {
+                        $cf[] = $bobot;
+                    } else {
+                        $sf[] = $bobot;
+                    }
+
+                    $hasil[$kasus->id][$alt->id]['gap'][$k->id]   = $gap;
+                    $hasil[$kasus->id][$alt->id]['bobot'][$k->id] = $bobot;
+                }
+
+                $nilaiCF = count($cf) ? array_sum($cf) / count($cf) : 0;
+                $nilaiSF = count($sf) ? array_sum($sf) / count($sf) : 0;
+
+                $hasil[$kasus->id][$alt->id]['cf'] = round($nilaiCF, 2);
+                $hasil[$kasus->id][$alt->id]['sf'] = round($nilaiSF, 2);
+                $hasil[$kasus->id][$alt->id]['nilai_akhir'] =
+                    round(($nilaiCF * 0.6) + ($nilaiSF * 0.4), 2);
+            }
+        }
+
+        $ringkasan = [];
+
+        foreach ($nilai as $kasus) {
+            $maxNilai = 0;
+            $rekomendasi = '';
+
+            foreach ($alternatif as $alt) {
+                $nilaiAkhir = $hasil[$kasus->id][$alt->id]['nilai_akhir'] ?? 0;
+
+                $ringkasan[$kasus->id]['nilai'][$alt->id] = $nilaiAkhir;
+
+                if ($nilaiAkhir > $maxNilai) {
+                    $maxNilai = $nilaiAkhir;
+                    $rekomendasi = $alt->alternatif;
+                }
             }
 
-            $hasil[$kasus->id][$alt->id]['gap'][$k->id]   = $gap;
-            $hasil[$kasus->id][$alt->id]['bobot'][$k->id] = $bobot;
+            $ringkasan[$kasus->id]['max'] = round($maxNilai, 2);
+            $ringkasan[$kasus->id]['rekomendasi'] = $rekomendasi;
         }
 
-        $nilaiCF = count($cf) ? array_sum($cf) / count($cf) : 0;
-        $nilaiSF = count($sf) ? array_sum($sf) / count($sf) : 0;
-
-        $hasil[$kasus->id][$alt->id]['cf'] = round($nilaiCF, 2);
-        $hasil[$kasus->id][$alt->id]['sf'] = round($nilaiSF, 2);
-        $hasil[$kasus->id][$alt->id]['nilai_akhir'] =
-            round(($nilaiCF * 0.6) + ($nilaiSF * 0.4), 2);
-    }
-}
-
-$ringkasan = [];
-
-foreach ($nilai as $kasus) {
-    $maxNilai = 0;
-    $rekomendasi = '';
-
-    foreach ($alternatif as $alt) {
-        $nilaiAkhir = $hasil[$kasus->id][$alt->id]['nilai_akhir'] ?? 0;
-
-        $ringkasan[$kasus->id]['nilai'][$alt->id] = $nilaiAkhir;
-
-        if ($nilaiAkhir > $maxNilai) {
-            $maxNilai = $nilaiAkhir;
-            $rekomendasi = $alt->alternatif;
-        }
-    }
-
-    $ringkasan[$kasus->id]['max'] = round($maxNilai, 2);
-    $ringkasan[$kasus->id]['rekomendasi'] = $rekomendasi;
-}
-
-return view(
-    'admin.rekomendasi.index',
-    compact(
-        'kriteria',
-        'alternatif',
-        'nilai',
-        'konversi',
-        'hasil',
-        'konversi_all',
-        'ringkasan'
-    )
-);
-
+        return view(
+            'admin.rekomendasi.index',
+            compact(
+                'kriteria',
+                'alternatif',
+                'nilai',
+                'konversi',
+                'hasil',
+                'konversi_all',
+                'ringkasan'
+            )
+        );
     }
 
     // Tampilkan form tambah data
